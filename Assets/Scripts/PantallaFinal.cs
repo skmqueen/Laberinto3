589713765238148aab49858d6851e765cde4c578
Iria <iriagb04@gmail.com>
2025-10-02 10:27:04 +0100

    Final acabado
    He acabado el final y agregado nuevas texturas

--- Assets/Scripts/PantallaFinal.cs
+++ Assets/Scripts/PantallaFinal.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class PantallaFinal : MonoBehaviour
 {
 
     [SerializeField]
     GameObject pantallaFinal;
+    [SerializeField]
+    TextMeshProUGUI textLabelTime;
 
+    float tiempoDePartida = 0.0f;
+    bool estaJugando = true;
 
+    private void Update()
+    {
+        if (estaJugando == true)
+        {
+            tiempoDePartida = tiempoDePartida + Time.deltaTime;
+
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -14,6 +27,9 @@ public class PantallaFinal : MonoBehaviour
             Debug.Log("Ha llegado a la meta");
             pantallaFinal.SetActive(true);
             other.GetComponent<MovimientoCaracol>().enabled = false;
+            estaJugando = false;
+            textLabelTime.text = tiempoDePartida.ToString();
+
         }
 
     }
